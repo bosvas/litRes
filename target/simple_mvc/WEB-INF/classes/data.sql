384ec48d41ecfd7fbde398c65757ccde5e9f169f
Boris Vasilev <7277387@gmail.com>
2022-11-29 00:22:35 +0200

    1_8.1 git bug fix

---
+++ target/simple_mvc/WEB-INF/classes/data.sql
@@ -0,0 +1,4 @@
+insert into books(author, title, size) values ('Dostoevskiy', 'Idiot', 500);
+insert into books(author, title, size) values ('Pushkin', 'Lukomorie', 200);
+insert into books(author, title, size) values ('Cierra and Baits', 'Learning JAVA', 700);
+insert into books(author, title, size) values ('Aditya Bhargava', 'Grokaem Alghoritms', 180);
